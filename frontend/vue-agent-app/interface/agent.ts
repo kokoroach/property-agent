e0a2eadad6ecd567ba6fd6d2d2c43b289e23cf86
kokoroach <jonel.getigan@gmail.com>
2025-11-13 14:26:19 +0800

    feat: initial form

---
+++ frontend/vue-agent-app/interface/agent.ts
@@ -0,0 +1,10 @@
+export interface Agent {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobileNumber: string,
+  isActive: boolean
+  created: Date,
+  updated: Date,
+}
